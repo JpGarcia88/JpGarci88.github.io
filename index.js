7ad179d218252e0d8611288eb4637662ee7b8a71
JpGarcia88 <johnpaulgarcia422@gmail.com>
2024-06-08 11:09:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -29,3 +29,11 @@ function hitdisLikes2() {
 }
 btndisLike1.addEventListener("click",hitdisLikes1)
 btndisLike2.addEventListener("click",hitdisLikes2)
+
+const submit = document.getElementById("submit")  
+const comment = document.getElementById("comment") 
+const commentbox = document.getElementById("commentbox")  
+function submitComment(){   
+  commentbox.textContent += comment.value.toString() + "\n" 
+  } 
+submit.addEventListener("click", submitComment)  
